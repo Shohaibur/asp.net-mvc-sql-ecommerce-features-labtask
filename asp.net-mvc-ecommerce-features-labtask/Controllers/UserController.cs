bb34b1fc515fcbe520ef80d19ac28ef28bdf1841
Shoaib <shoaibrochi9@gmail.com>
2023-11-15 04:14:44 +0600

    Added: Userrepository,login view, modified : usercontroller, user index

--- asp.net-mvc-ecommerce-features-labtask/Controllers/UserController.cs
+++ asp.net-mvc-ecommerce-features-labtask/Controllers/UserController.cs
@@ -33,5 +33,35 @@ namespace asp.net_mvc_ecommerce_features_labtask.Controllers
         {
             return  View();
         }
+
+       
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string username, int userid)
+        {
+            using (var db = new EcommerceDBEntities())
+            {
+                var user = db.Users.FirstOrDefault(u => u.UserName == username && u.UserId == userid);
+
+                if (user != null)
+                {
+                    // Valid credentials, redirect to Home or a specific page
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    // Invalid credentials, display error message or handle accordingly
+                    ViewBag.ErrorMessage = "Invalid username or userid";
+                    return View();
+                }
+            }
+        }
+
     }
 }
---
+++ asp.net-mvc-ecommerce-features-labtask/Repositories/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using asp.net_mvc_ecommerce_features_labtask.Models;
+using System.Linq;
+
+public class UserRepository
+{
+    private readonly EcommerceDBEntities _context;
+
+    public UserRepository(EcommerceDBEntities context)
+    {
+        _context = context;
+    }
+
+    public User GetUserByCredentials(int userId, string username)
+    {
+        return _context.Users.FirstOrDefault(u => u.UserId == userId && u.UserName == username);
+    }
+}
